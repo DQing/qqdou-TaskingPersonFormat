ad1bd397ad0d01c40144349c969d307b3cbd395e
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-17 20:39:46 +0800

    add person entity
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

---
+++ src/main/java/com/thoughtworks/Person.java
@@ -0,0 +1,24 @@
+package com.thoughtworks;
+
+import java.util.Objects;
+
+class Person {
+    private String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
