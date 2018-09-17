59812d38895a4031958fed1675feed9b6a3cd9ad
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-17 20:31:17 +0800

    finish get format name list
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/main/java/com/thoughtworks/Application.java
+++ src/main/java/com/thoughtworks/Application.java
@@ -1,4 +1,17 @@
 package com.thoughtworks;
 
-public class Application {
+import java.util.ArrayList;
+import java.util.List;
+
+class Application {
+    List<String> getFormatNames(String input) {
+        List<String> result = new ArrayList<>();
+        String[] names = input.split(",");
+        for (String name : names) {
+            String[] splitName = name.split(" ");
+            String finalName = splitName[0].toUpperCase() + "," + splitName[1].toUpperCase();
+            result.add(finalName);
+        }
+        return result;
+    }
 }
--- src/test/java/com/thoughtworks/ApplicationTest.java
+++ src/test/java/com/thoughtworks/ApplicationTest.java
@@ -1,4 +1,21 @@
 package com.thoughtworks;
 
-public class ApplicationTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+class ApplicationTest {
+    @Test
+    void should_get_format_list() {
+        //given
+        String input = "huang lizheng,zuo peixi";
+        //when
+        List<String> stringList = new Application().getFormatNames(input);
+        //then
+        List<String> excepted = Arrays.asList("HUANG,LIZHENG", "ZUO,PEIXI");
+        assertIterableEquals(excepted, stringList);
+    }
 }
